b6435a3b1ee714471ce7062a653d242357a2a3f0
Adam <adam.rp.nimmo@gmail.com>
2021-11-28 18:21:55 +0000

    rejig dock

--- macos/dock.sh
+++ macos/dock.sh
@@ -2,13 +2,12 @@
 
 dockutil --no-restart --remove all
 dockutil --no-restart --add "/Applications/iTerm.app"
+dockutil --no-restart --add "/Applications/Ray.app"
 dockutil --no-restart --add "/Applications/Tinkerwell.app"
 dockutil --no-restart --add "/Applications/Firefox Developer Edition.app"
 dockutil --no-restart --add "/Applications/Visual Studio Code - Insiders.app"
 dockutil --no-restart --add "/Applications/TablePlus.app"
-dockutil --no-restart --add "/Applications/Querious.app"
 dockutil --no-restart --add "/Applications/Slack.app"
-dockutil --no-restart --add "/Applications/Spotify.app"
 
 killall Dock
 
